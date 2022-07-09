a694d19feefb7be5ed66961430c9764ffe6266bd
Kayton <kt.kaytonheng@gmail.com>
2022-07-09 14:38:27 +0800

    Antd+router setup

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '@/images/home/logo.svg';
+
+const Home = () => {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
+    )
+}
+
+export default Home;
